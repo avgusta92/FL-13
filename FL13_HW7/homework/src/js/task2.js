5343ca4640351142aed236450e9c86341d03be99
Anastasiia Vatsyk <58269603+avgusta92@users.noreply.github.com>
2020-04-28 22:00:10 +0300

    HW7

--- FL13_HW7/homework/src/js/task2.js
+++ FL13_HW7/homework/src/js/task2.js
@@ -23,40 +23,28 @@ if (startGame) {
     let possiblePrize = [NUMBER100, NUMBER50, NUMBER25];
 
     do {
-      // Данные для 2 уровня и выше
       if (level >= NUMBER2) {
         rangeNumber = rangeNumber + NUMBER5;
       }
-
-      // Определяем рандомное число
       randomNumber = parseInt(Math.random() * rangeNumber);
-
       event = 0;
-
-      // Угадываем рандомное число
       do {
         nextLevel = false;
         possiblePrize =
-          level === 1 ? possiblePrize : possiblePrize.map((i) => i * NUMBER2);
-        enteredNumber = parseInt(
-          prompt(`
-Choose a roulette pocket number from 0 то ${rangeNumber}
+        level === 1 ? possiblePrize : possiblePrize.map((i) => i * NUMBER2);
+        enteredNumber = parseInt(prompt(`Choose a roulette pocket number from 0 то ${rangeNumber}
 Attempts left: ${leftAttempts[event]}
 Total prize: ${userPrize}$
-Possible prize on current attempt: ${possiblePrize[event]}$`)
-        );
+Possible prize on current attempt: ${possiblePrize[event]}$`));
         if (enteredNumber === randomNumber) {
           userPrize = userPrize + possiblePrize[event];
-          nextLevel = confirm(`
-Congratulation, you won! Your prize is: ${userPrize}$. 
+          nextLevel = confirm(`Congratulation, you won! Your prize is: ${userPrize}$. 
 Do you want to continue?`);
         }
         event++;
       } while (event < NUMBER3 && enteredNumber !== randomNumber);
-
       level++;
     } while (nextLevel);
-
     alert(`Thank you for your participation. Your prize is: ${userPrize}$`);
     restartGame = confirm(`Do you want to play again?`);
   } while (restartGame);
