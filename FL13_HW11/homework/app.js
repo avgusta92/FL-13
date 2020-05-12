33c8f043aacb7fa9458559d64c0a91e9e8ab1b33
Anastasiia Vatsyk <58269603+avgusta92@users.noreply.github.com>
2020-05-12 23:51:06 +0300

    Update app.js

--- FL13_HW11/homework/app.js
+++ FL13_HW11/homework/app.js
@@ -52,20 +52,18 @@ const data = [
 
 const rootNode = document.getElementById('root');
 
-// TODO: your code goes here
-
 rootNode.className = 'main-wrap';
 
 let contextMenu = document.createElement('div');
 contextMenu.className = `context-menu`;
 rootNode.appendChild(contextMenu);
 
-let contextMenuRenameItem = document.createElement('lable');
+let contextMenuRenameItem = document.createElement('label');
 contextMenuRenameItem.className = `context-menu-item`;
 contextMenuRenameItem.innerHTML = `Rename`;
 contextMenu.appendChild(contextMenuRenameItem);
 
-let contextMenuDeleteItem = document.createElement('lable');
+let contextMenuDeleteItem = document.createElement('label');
 contextMenuDeleteItem.className = `context-menu-item`;
 contextMenuDeleteItem.innerHTML = `Delete item`;
 contextMenu.appendChild(contextMenuDeleteItem);
@@ -82,99 +80,74 @@ function folderShow() {
 
 function onContextMenu(event) {
   event.currentTarget.className = `folder `;
-
   curentActiveFolder = event.currentTarget;
-
   contextMenu.style.top = event.clientY + 'px';
   contextMenu.style.left = event.clientX + 'px';
-
   folderShow()
-
   event.stopPropagation();
   return false;
 }
 
 function deleteItem(event) {
   let parentFolder = curentActiveFolder.parentElement;
-
   parentFolder.remove()
-
-
   if (parentFolder.querySelector('folder') === null) {
-    let emptyFolder = document.createElement('lable');
-    emptyFolder.className = `lable empty-lable`;
+    let emptyFolder = document.createElement('label');
+    emptyFolder.className = `label empty-label`;
     emptyFolder.innerHTML = `Folder is empty`;
     parentFolder.appendChild(emptyFolder);
-
   }
-
   contextMenyHide()
-
   event.stopPropagation();
   return false;
 }
 
 function renameItem(event) {
   curentActiveFolder.lastChild.style.display = 'none';
-
   let setInput = document.createElement('input');
   setInput.setAttribute('type', 'text');
   setInput.className = `folder`;
+  setInput.onchange = setNewName;
   curentActiveFolder.appendChild(setInput);
   setInput.focus();
-
   contextMenyHide()
-
   event.stopPropagation();
   return false;
 }
 
 function setNewName(event) {
   curentActiveFolder.lastChild.remove();
-  curentActiveFolder.querySelector('lable').innerHTML = event.target.value;
-
-  let lable = document.createElement('lable');
-  lable.className = `lable`;
-  lable.innerHTML = event.target.value;
-  curentActiveFolder.appendChild(lable);
+  curentActiveFolder.querySelector('label').innerHTML = event.target.value;
+  let label = document.createElement('label');
+  label.className = `label`;
+  label.innerHTML = event.target.value;
+  curentActiveFolder.appendChild(label);
 }
 
-let getInput = document.querySelector('body');
-
-getInput.onchange = setNewName;
-
-//  ПОДПИСЫВАЕТСЯ И ВЫЗЫВАЕТ ФУНКЦИЮ НА ВСЕ ИВЕНТЫ СО ЗНАЧЕНИЕМ CLICK
 document.addEventListener('click', function (event) {
   contextMenu.style.display = 'none';
   event.stopPropagation();
   return false;
 });
 
-
 function newItem(value, place) {
-
-  // ООБЕРКА ДЛЯ НЕСКОЛЬКИХ ПАПОК-ОБЕРТОК
   let wrap = document.createElement('div');
   wrap.className = `wrap`;
   place.appendChild(wrap);
 
-  // ПАПКА-ОБЕРКА (ВНУТРИ ИКОНКА + НАЗВАНИЕ)
   let folder = document.createElement('div');
   folder.className = `folder`;
   wrap.appendChild(folder);
 
-  // ИКОНКА 
   let icon = document.createElement('i');
   icon.innerHTML = 'folder';
   folder.appendChild(icon);
 
-  // НАЗВАНИЕ
-  let lable = document.createElement('lable');
-  lable.className = `lable`;
-  lable.innerHTML = value.title;
-  folder.appendChild(lable);
+  let label = document.createElement('label');
+  label.className = `label`;
+  label.innerHTML = value.title;
+  folder.appendChild(label);
 
-  // ЕСЛИ ЭТО ПАПКА ТО СЕТАЕМ ИКОНКУ ПАПКИ ЕСЛИ ЭТО ФАЙЛ СЕТАЕМ ИКОНКУ ФАЙЛА
   if (value.folder === true) {
     icon.className = `icon icon-folder material-icons`;
     icon.innerHTML = 'folder';
@@ -190,17 +163,16 @@ function newItem(value, place) {
     newWrap.className = `wrap`;
     wrap.appendChild(newWrap);
 
-    let emptyFolder = document.createElement('lable');
-    emptyFolder.className = `lable empty-lable`;
+    let emptyFolder = document.createElement('label');
+    emptyFolder.className = `label empty-label`;
     emptyFolder.innerHTML = `Folder is empty`;
     newWrap.appendChild(emptyFolder);
   }
 
-  // ОТКРЫТИЕ ПАПКИ
   function openFolder() {
     let childrenArray = this.children;
 
-    contextMenu.style.display = 'none'; //  ПРЯЧЕТСЯ КОНТЕКСТНОЕ МЕНЮ (НУЖНО ПЕРЕДЕЛАТЬ)
+    contextMenu.style.display = 'none';
 
     for (let i = 0; i < childrenArray.length; i++) {
       let children = childrenArray[i];
@@ -212,11 +184,10 @@ function newItem(value, place) {
     }
   }
 
-  // ЗАКРЫТИЕ ПАПКИ
   function closeFolder() {
     let childrenArray = this.children;
 
-    contextMenu.style.display = 'none'; //  ПРЯЧЕТСЯ КОНТЕКСТНОЕ МЕНЮ (НУЖНО ПЕРЕДЕЛАТЬ)
+    contextMenu.style.display = 'none';
 
     for (let i = 0; i < childrenArray.length; i++) {
       let children = childrenArray[i];
@@ -228,7 +199,6 @@ function newItem(value, place) {
     }
   }
 
-  // ФУНКЦИЯ ПРИ КЛИКЕ - КАК ОТОБРАЖАТЬ ПАПКУ (ОТКРЫТОЙ ИЛИ ЗАКРЫТОЙ)
   function displayFolderFunc(event) {
     let iconFolder = wrap.children[0].children[0];
     if (iconFolder.innerHTML === 'folder') {
